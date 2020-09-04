fea976187e50aff084be75871c3b1d4083e1f320
Richard Bao <richardbao419@gmail.com>
2020-09-04 03:14:03 -0700

    update

--- controller/train.py
+++ controller/train.py
@@ -1,69 +1,63 @@
 import numpy as np
 import gym
 
-from keras.models import Sequential, Model
-from keras.layers import Dense, Activation, Flatten, Input, Concatenate
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
 from keras.optimizers import Adam
 
-from rl.agents import DDPGAgent
+from rl.agents.dqn import DQNAgent
+from rl.policy import BoltzmannQPolicy
 from rl.memory import SequentialMemory
-from rl.random import OrnsteinUhlenbeckProcess
 
+import noise
 
 ENV_NAME = 'ligo_environment:two-stage-v1'
+seismic = noise.generate_noise()
 
 
 # Get the environment and extract the number of actions.
-env = gym.make(ENV_NAME)
+env = gym.make(ENV_NAME, seismic=seismic)
 np.random.seed(123)
 env.seed(123)
-assert len(env.action_space.shape) == 1
-nb_actions = env.action_space.shape[0]
+nb_actions = env.action_space.n
 
 # Next, we build a very simple model.
-actor = Sequential()
-actor.add(Flatten(input_shape=(1,) + env.observation_space.shape))
-actor.add(Dense(16))
-actor.add(Activation('relu'))
-actor.add(Dense(16))
-actor.add(Activation('relu'))
-actor.add(Dense(16))
-actor.add(Activation('relu'))
-actor.add(Dense(nb_actions))
-actor.add(Activation('linear'))
-print(actor.summary())
+model = Sequential()
+# model.add(Flatten(input_shape=(6, 1) + env.observation_space.shape))
+model.add(Flatten(input_shape=(1,6,1)))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(16))
+model.add(Activation('relu'))
+model.add(Dense(nb_actions))
+model.add(Activation('linear'))
 
-action_input = Input(shape=(nb_actions,), name='action_input')
-observation_input = Input(shape=(1,) + env.observation_space.shape, name='observation_input')
-flattened_observation = Flatten()(observation_input)
-x = Concatenate()([action_input, flattened_observation])
-x = Dense(32)(x)
-x = Activation('relu')(x)
-x = Dense(32)(x)
-x = Activation('relu')(x)
-x = Dense(32)(x)
-x = Activation('relu')(x)
-x = Dense(1)(x)
-x = Activation('linear')(x)
-critic = Model(inputs=[action_input, observation_input], outputs=x)
-print(critic.summary())
+# # Add a LSTM layer with 128 internal units.
+# model.add(keras.layers.LSTM(128))
+# # Add a Dense layer with 10 units.
+# model.add(keras.layers.Dense(10))
+
+print(model.summary())
 
 # Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
 # even the metrics!
-memory = SequentialMemory(limit=100000, window_length=1)
-random_process = OrnsteinUhlenbeckProcess(size=nb_actions, theta=.15, mu=0., sigma=.3)
-agent = DDPGAgent(nb_actions=nb_actions, actor=actor, critic=critic, critic_action_input=action_input,
-                  memory=memory, nb_steps_warmup_critic=100, nb_steps_warmup_actor=100,
-                  random_process=random_process, gamma=.99, target_model_update=1e-3)
-agent.compile(Adam(lr=.001, clipnorm=1.), metrics=['mae'])
+memory = SequentialMemory(limit=50000, window_length=1)
+policy = BoltzmannQPolicy()
+dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10,
+               target_model_update=1e-2, policy=policy)
+dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 
 # Okay, now it's time to learn something! We visualize the training here for show, but this
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
-agent.fit(env, nb_steps=50000, visualize=True, verbose=1, nb_max_episode_steps=200)
+# dqn.fit(env, nb_steps=50000, visualize=True, verbose=2)
+dqn.fit(env, nb_steps=50000, visualize=True, verbose=1)
 
 # After training is done, we save the final weights.
-agent.save_weights('saved_model/{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+dqn.save_weights('saved_model/dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
 
 # Finally, evaluate our algorithm for 5 episodes.
-agent.test(env, nb_episodes=5, visualize=True, nb_max_episode_steps=200)
+dqn.test(env, nb_episodes=5, visualize=True)
+
